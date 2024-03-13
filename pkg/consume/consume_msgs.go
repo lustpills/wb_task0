a3b686a8521ffd0bc180fd312aa210db3f3e7210
lustpills <andrey.borisov14@mail.ru>
2024-03-13 14:27:18 +0300

    fixed comments

--- pkg/consume/consume_msgs.go
+++ pkg/consume/consume_msgs.go
@@ -39,11 +39,11 @@ func (c *Consumer) Consuming(ctx context.Context) {
 				log.Println(err)
 			} else {
 				fmt.Println("Sucsessfully stored an order: ", order_id)
+
+				// second caching an order
 				caching.MyCache.Set(order_id, new_order)
 			}
 
-			// second caching an order
-			// /caching.MyCache.Set(order_id, new_order)
 			messageCounter++
 			msg.Ack()
 		}
